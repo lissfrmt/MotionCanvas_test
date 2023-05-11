3f5aba057adfcf909b89e72134895572592932c3
Eilis Se <sem-Liza@mail.ru>
2023-05-11 15:49:33 +0300

    Revert "Initial commit"
    This reverts commit eaa54e1430a327416390b44e037fcd6618b7240c.

--- motion-canvas/src/projects/block1/lesson1/block1.lesson1.ts
+++
@@ -1,9 +0,0 @@
-import { makeProject } from "@motion-canvas/core";
-import "@/global.css";
-import scene1 from "./scenes/scene1?scene";
-import scene2 from "./scenes/scene2?scene";
-
-export default makeProject({
-  name: "block1.lesson1",
-  scenes: [scene1, scene2],
-});
